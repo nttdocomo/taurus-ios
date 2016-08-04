2d62b7f1caf4b0551e2f3cdb380d2b2944979436
ouyi <ouiy@didichuxing.com>
2016-08-04 18:28:34 +0800

    update

--- examples/virtual-dom/vdom.js
+++ examples/virtual-dom/vdom.js
@@ -1,4 +1,4 @@
-define(function(require){
+define(function (require) {
     var h = require("../../src/virtual-dom/h"),
     diff = require("../../src/virtual-dom/diff"),
     createElement = require("../../src/virtual-dom/create-element"),
@@ -8,6 +8,12 @@ define(function(require){
     var leftNode = new Function('h','return ' + dom2hscript.parseHTML('<ul><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li></ul>'))(h)
     var rightNode = eval(dom2hscript.parseHTML('<ul><li>1</li><li>2</li><li>3</li><li>4</li><li>10</li></ul>'))
     // Render the left node to a DOM node
+    console.log(h('div', {
+        className:'title',
+        namespace: "http://www.w3.org/2000/svg"
+    }, [h('div',{
+        className: 'description'
+    })]))
     var rootNode = createElement(leftNode)
     document.body.appendChild(rootNode)
 
--- src/dom/element.js
+++ src/dom/element.js
@@ -5,20 +5,58 @@
 ;(function (root, factory) {
   if (typeof define === 'function') {
     if (define.amd) {
-      define(['../core/define', 'class', 'backbone', 'backbone-super', 'underscore', '../virtual-dom/h', '../virtual-dom/create-element'], factory)
+      define(['../core/define', 'class', 'backbone', 'backbone-super', 'underscore', '../virtual-dom/h', '../virtual-dom/create-element', '../mixin/identifiable'], factory)
     }
     if (define.cmd) {
       define(function (require, exports, module) {
-        return factory(require('../core/define'), require('class'), require('backbone'), require('backbone-super'), require('underscore'), require('../virtual-dom/h'), require('../virtual-dom/create-element'))
+        return factory(require('../core/define'), require('class'), require('backbone'), require('backbone-super'), require('underscore'), require('../virtual-dom/h'), require('../virtual-dom/create-element'), require('../mixin/identifiable'))
       })
     }
   } else if (typeof module === 'object' && module.exports) {
-    module.exports = factory(require('../core/define'), require('class'), require('backbone'), require('backbone-super'), require('underscore'), require('../virtual-dom/h'), require('../virtual-dom/create-element'))
+    module.exports = factory(require('../core/define'), require('class'), require('backbone'), require('backbone-super'), require('underscore'), require('../virtual-dom/h'), require('../virtual-dom/create-element'), require('../mixin/identifiable'))
   }
-}(this, function (define, Class, Backbone, inherits, _, h, createElement) {
-  var Element = define(Class, {
-    constructor: function () {}
+}(this, function (define, Class, Backbone, inherits, _, h, createElement, Identifiable) {
+  var Element = define('Tau.dom.Element', Class, {
+    constructor: function (dom, vdom) {
+      if (typeof dom === 'string') {
+        dom = document.getElementById(dom)
+      }
+      if (!dom) {
+        throw new Error('Invalid domNode reference or an id of an existing domNode: ' + dom)
+      }
+      /**
+       * The DOM element
+       * @property dom
+       * @type HTMLElement
+       */
+      this.dom = dom
+      if (vdom) {
+        this.vdom = vdom
+      }
+
+      this.getUniqueId()
+    },
+
+    getUniqueId: function () {
+      var id = this.id
+      var dom
+
+      if (!id) {
+        dom = this.dom
+
+        if (dom.id.length > 0) {
+          this.id = id = dom.id
+        } else {
+          dom.id = id = Identifiable.prototype.getUniqueId.call(this)
+        }
+
+        Element.cache[id] = this
+      }
+
+      return id
+    }
   }, {
+    cache: {},
     hyperScript: function (attributes) {
       var me = this
       var tag = attributes.tag
@@ -39,96 +77,98 @@
     },
     create: function (attributes, domNode) {
       console.log(arguments)
-      var element
-      element = createElement(this.hyperScript(attributes))
+      var vdom = this.hyperScript(attributes)
+      var element = createElement(this.hyperScript(attributes))
+      var instance = new Element(element, vdom)
       return element
-      /* var ATTRIBUTES = this.CREATE_ATTRIBUTES
-      var element, elementStyle, tag, value, name, i, ln
+    /* var ATTRIBUTES = this.CREATE_ATTRIBUTES
+    var element, elementStyle, tag, value, name, i, ln
 
-      if (!attributes) {
-        attributes = {}
-      }
+    if (!attributes) {
+      attributes = {}
+    }
 
-      if (attributes.isElement) {
-        return attributes.dom
-      } else if ('nodeType' in attributes) {
-        return attributes
-      }
+    if (attributes.isElement) {
+      return attributes.dom
+    } else if ('nodeType' in attributes) {
+      return attributes
+    }
 
-      if (typeof attributes === 'string') {
-        return document.createTextNode(attributes)
-      }
+    if (typeof attributes === 'string') {
+      return document.createTextNode(attributes)
+    }
 
-      tag = attributes.tag
+    tag = attributes.tag
 
-      if (!tag) {
-        tag = 'div'
-      }
-      if (attributes.namespace) {
-        element = document.createElementNS(attributes.namespace, tag)
-      } else {
-        element = document.createElement(tag)
-      }
-      elementStyle = element.style
+    if (!tag) {
+      tag = 'div'
+    }
+    if (attributes.namespace) {
+      element = document.createElementNS(attributes.namespace, tag)
+    } else {
+      element = document.createElement(tag)
+    }
+    elementStyle = element.style
 
-      for (name in attributes) {
-        if (name !== 'tag') {
-          value = attributes[name]
+    for (name in attributes) {
+      if (name !== 'tag') {
+        value = attributes[name]
 
-          switch (name) {
-            case ATTRIBUTES.style:
-              if (typeof value === 'string') {
-                element.setAttribute(name, value)
-              }else {
-                for (i in value) {
-                  if (value.hasOwnProperty(i)) {
-                    elementStyle[i] = value[i]
-                  }
+        switch (name) {
+          case ATTRIBUTES.style:
+            if (typeof value === 'string') {
+              element.setAttribute(name, value)
+            }else {
+              for (i in value) {
+                if (value.hasOwnProperty(i)) {
+                  elementStyle[i] = value[i]
                 }
               }
-              break
+            }
+            break
 
-            case ATTRIBUTES.className:
-            case ATTRIBUTES.cls:
-              element.className = value
-              break
+          case ATTRIBUTES.className:
+          case ATTRIBUTES.cls:
+            element.className = value
+            break
 
-            case ATTRIBUTES.classList:
-              element.className = value.join(' ')
-              break
+          case ATTRIBUTES.classList:
+            element.className = value.join(' ')
+            break
 
-            case ATTRIBUTES.text:
-              element.textContent = value
-              break
+          case ATTRIBUTES.text:
+            element.textContent = value
+            break
 
-            case ATTRIBUTES.hidden:
-              if (value) {
-                element.style.display = 'none'
-              }
-              break
+          case ATTRIBUTES.hidden:
+            if (value) {
+              element.style.display = 'none'
+            }
+            break
 
-            case ATTRIBUTES.html:
-              element.innerHTML = value
-              break
+          case ATTRIBUTES.html:
+            element.innerHTML = value
+            break
 
-            case ATTRIBUTES.children:
-              for (i = 0, ln = value.length; i < ln; i++) {
-                element.appendChild(this.create(value[i], true))
-              }
-              break
+          case ATTRIBUTES.children:
+            for (i = 0, ln = value.length; i < ln; i++) {
+              element.appendChild(this.create(value[i], true))
+            }
+            break
 
-            default:
-              element.setAttribute(name, value)
-          }
+          default:
+            element.setAttribute(name, value)
         }
       }
+    }
 
-      if (domNode) {
-        return element
-      }else {
-        return this.get(element)
-      }*/
+    if (domNode) {
+      return element
+    }else {
+      return this.get(element)
+    }*/
     }
   })
+  Element.mixin(Identifiable)
   return Element
 }))
